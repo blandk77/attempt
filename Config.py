9de592f918fb4e14cf7b4ebfbff50f914b2de39f
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-02-04 11:21:27 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,12 @@
+# config.py
+
+class Config:
+    # Telegram bot credentials
+    TELEGRAM_API_ID = '3847632'
+    TELEGRAM_API_HASH = '1a9708f807ddd06b10337f2091c67657'
+    TELEGRAM_BOT_TOKEN = '6368528705:AAGFzGXZbyR_Puuusj_Af-2WGhJCrUMe1Sw'
+
+    # Crunchyroll credentials
+    CR_EMAIL = 'dhanush6010@gmail.com'
+    CR_PASSWORD = 'dhanush123'
+  
